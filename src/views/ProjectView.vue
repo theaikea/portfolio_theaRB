a004627cab1f1fa723b2999e8ead4ac1cf874a68
theaikea <thea0604@gmail.com>
2025-02-19 00:44:12 +0100

    padding

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -26,31 +26,31 @@ export default {
       {{ project.descriptionText }} 
     </p>
   
-    <img v-if="project.titleImage" class="mt-5v w-4/5 sm:w-auto col-start-1" :src="project.titleImage" alt="">
-    <video v-else-if="project.video" :src="project.video" class="mt-5 sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
+    <img v-if="project.titleImage" class="mt-5 w-full pr-6 sm:w-auto col-start-1" :src="project.titleImage" alt="">
+    <video v-else-if="project.video" :src="project.video" class="mt-5 sm:w-auto col-start-1 w-4/5 pr-6" autoplay muted loop></video>
   
-    <img v-if="project.Image2" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" :src="project.Image2" alt="">
-    <video v-else-if="project.video2" :src="project.video2" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
+    <img v-if="project.Image2" class="mt-5 w-full pr-6 sm:w-auto col-start-1 w-4/5" :src="project.Image2" alt="">
+    <video v-else-if="project.video2" :src="project.video2" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" autoplay muted loop></video>
   
-    <p v-if="project.description2Text" class="poppins text-lg w-full sm:w-1/3 mt-5 w-4/5"> 
+    <p v-if="project.description2Text" class="poppins text-lg pr-6 w-full sm:w-1/3 mt-5 w-4/5"> 
       {{ project.description2Text }} 
     </p>
   
-    <img v-if="project.Image3" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" :src="project.Image3" alt="">
+    <img v-if="project.Image3" class="mt-5 w-full pr-6 sm:w-auto col-start-1 w-4/5" :src="project.Image3" alt="">
     <video v-else-if="project.video3" :src="project.video3" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
   
-    <img v-if="project.image4" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" :src="project.image4" alt="">
-    <video v-else-if="project.video4" :src="project.video4" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
+    <img v-if="project.image4" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" :src="project.image4" alt="">
+    <video v-else-if="project.video4" :src="project.video4" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" autoplay muted loop></video>
   
-    <p v-if="project.description3Text" class="poppins text-lg w-full sm:w-1/3 mt-5 w-4/5"> 
+    <p v-if="project.description3Text" class="poppins text-lg w-full sm:w-1/3 mt-5 w-4/5 pr-6"> 
       {{ project.description3Text }} 
     </p>
   
-    <img v-if="project.image5" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" :src="project.image5" alt="">
-    <video v-else-if="project.video5" :src="project.video5" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
+    <img v-if="project.image5" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" :src="project.image5" alt="">
+    <video v-else-if="project.video5" :src="project.video5" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" autoplay muted loop></video>
   
-    <img v-if="project.image6" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" :src="project.image6" alt="">
-    <video v-else-if="project.video6" :src="project.video6" class="mt-5 w-full sm:w-auto col-start-1 w-4/5" autoplay muted loop></video>
+    <img v-if="project.image6" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" :src="project.image6" alt="">
+    <video v-else-if="project.video6" :src="project.video6" class="mt-5 w-full sm:w-auto col-start-1 w-4/5 pr-6" autoplay muted loop></video>
   
   </div>
 </template>
